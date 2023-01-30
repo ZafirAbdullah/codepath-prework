009acbc580f53be79f875dc9d5ec201e76153e27
ZafirAbdullah <108569152+ZafirAbdullah@users.noreply.github.com>
2023-01-29 21:09:41 -0500

    Added prework files to repository

---
+++ IntroStudent/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  IntroStudent
+//
+//  Created by Zafir Abdullah on 1/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    @IBOutlet weak var numberofPetsLabel: UILabel!
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        numberofPetsLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). It is \(morePetsSwitch.isOn) that I want more pets."
+        print(introduction)
+        
+        let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Hi, Nice to meet you!", style: .default, handler: nil)
+        
+        alertController.addAction(action)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
